7e754c93ec02b6ba42704b6dac8400ac536dca95
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-11-11 16:43:53 +0530

    use of public keyword

---
+++ Keywords/PUBLIC/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass(10);
+        myObject.printNumber();  // Calling the public method from another package
+    }
+}
---
+++ Keywords/PUBLIC/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public int number;
+
+    public MyClass(int number) {
+        this.number = number;
+    }
+
+    public void printNumber() {
+        System.out.println("The number is: " + number);
+    }
+}
